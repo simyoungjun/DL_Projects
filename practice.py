bbdb44426416f8180b4d9f6eb0f6546f85fedab0
SIM <simyoungjun@gmail.com>
2025-01-18 22:08:12 +0900

    1

--- practice.py
+++
@@ -1,9 +0,0 @@
-#%
-import torch
-import os
-print(torch.cuda.is_available())
-device = torch.device('cuda:2') if torch.cuda.is_available() else torch.device('cpu')
-
-# 2번 GPU만 사용하고 싶은 경우 예시(cuda:0에 지정)
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
